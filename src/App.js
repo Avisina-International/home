563728c5274d53b15a7937a4c09a1940d3111a81
shahoodzee <shahood.bin.amir@gmail.com>
2024-06-04 01:55:48 -0700

    * Completed the Email Sending Functionality in Contact Us Section * Added a Banner component in the application * Fixed the navbar color according to Banner * Tested the code on local

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import MyNavbar from './components/navbar';
 import MyAlert from './components/alert';
 import MyCrousel from './components/crousel';
 import MyContactUs from './components/contactUs';
-
+import Banner from './components/banner';
 function App() {
   return (
     <div className="App">
       <MyAlert text="Welcome to Avisina International" />
       <MyNavbar />
+      <Banner />
       <MyCrousel />
       <MyContactUs />
     </div>
--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import {React , useState} from 'react'
+import emailjs from '@emailjs/browser';
 import {
   Card,
   CardBody,
@@ -6,21 +7,66 @@ import {
   CardSubtitle,
   CardText,
   Button,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
 } from 'reactstrap';
 import '../App.css'
 
 
 function MyContactUs() {
+
+  const [formData, setFormData] = useState({
+    FirstName: '',
+    LastName: '',
+    Email: '',
+    Phone: ''
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    const newErrors = {};
+    if (!formData.FirstName) newErrors.FirstName = 'First Name is required';
+    if (!formData.LastName) newErrors.LastName = 'Last Name is required';
+    if (!formData.Email) {
+      newErrors.Email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(formData.Email)) {
+      newErrors.Email = 'Email is invalid';
+    }
+    if (!formData.Phone) newErrors.Phone = 'Phone Number is required';
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formData, 'YOUR_USER_ID')
+        .then((response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        }, (err) => {
+          console.log('FAILED...', err);
+        });
+    }
+  };
+
   return (
     <div className='contactus--section' id='contact'>
       <div className='container'>
         <Card
           body
           outline
-          style={{
-            width: '18rem',
-            alignItems: 'center'
-          }}
         >
           <CardBody>
             <CardTitle tag="h5">
@@ -30,14 +76,105 @@ function MyContactUs() {
               className="mb-2 text-muted"
               tag="h6"
             >
-              Card subtitle
+              Contact us at our email for further queries.
             </CardSubtitle>
             <CardText>
-              Some quick example text to build on the card title and make up the bulk of the card‘s content.
+              We are also actively available on social media platforms
             </CardText>
-            <Button>
-              Send
-            </Button>
+
+
+
+            <Form onSubmit={handleSubmit}>
+              <Row>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="FirstName">First Name</Label>
+                    <Input
+                      id="FirstName"
+                      name="FirstName"
+                      placeholder="i.e Jhon"
+                      type="text"
+                      value={formData.FirstName}
+                      onChange={handleChange}
+                    />
+                    {errors.FirstName && <div className="text-danger">{errors.FirstName}</div>}
+                  </FormGroup>
+                </Col>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="LastName">Last Name</Label>
+                    <Input
+                      id="LastName"
+                      name="LastName"
+                      placeholder="i.e Doe"
+                      type="text"
+                      value={formData.LastName}
+                      onChange={handleChange}
+                    />
+                    {errors.LastName && <div className="text-danger">{errors.LastName}</div>}
+                  </FormGroup>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="Email">Email</Label>
+                    <Input
+                      id="Email"
+                      name="Email"
+                      placeholder="JhoneDoe@example.com"
+                      type="email"
+                      value={formData.Email}
+                      onChange={handleChange}
+                    />
+                    {errors.Email && <div className="text-danger">{errors.Email}</div>}
+                  </FormGroup>
+                </Col>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="Phone">Phone Number</Label>
+                    <Input
+                      id="Phone"
+                      name="Phone"
+                      placeholder="i.e 123456789"
+                      type="text"
+                      value={formData.Phone}
+                      onChange={handleChange}
+                    />
+                    {errors.Phone && <div className="text-danger">{errors.Phone}</div>}
+                  </FormGroup>
+                </Col>
+              </Row>
+
+              <Row className="justify-content-center">
+                <Col md={6} style={{ alignItems: 'center' }}>
+                  <FormGroup>
+                    <Label for="dropdown">Subject ...</Label>
+                    <div className="dropdown">
+                      <button
+                        id="dropdown"
+                        className="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Dropdown button
+                      </button>
+                      <ul className="dropdown-menu dropdown-menu-dark">
+                        <li>   Surgical Tools</li>
+                        <li>   Barbery Catalogue</li>
+                        <li>   Other</li>
+                      </ul>
+                    </div>
+                  </FormGroup>
+                </Col>
+              </Row>
+
+              <Button type="submit">Send</Button>
+            </Form>
+
+
           </CardBody>
         </Card>
       </div>
